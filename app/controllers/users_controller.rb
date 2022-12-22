876085572707035e2d7f07515f161f7d823132b8
EC2 Default User <ec2-user@ip-172-31-41-243.ap-northeast-1.compute.internal>
2022-12-22 23:28:07 +0000

    [fix]ログイン機能不具合修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,24 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @book = @user.books
+    @book = @user.books 
+    
   end 
   
   def edit
-    @user = User.find(paarms[:id])
+    @user = User.find(params[:id])
   end 
   
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
   end 
   
   private
   
   def user_params
-  params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :introduction)
   end
   
   
