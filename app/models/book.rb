bf2a97b29329ada8415f7fc8fa37a6aaf7319f68
EC2 Default User <ec2-user@ip-172-31-41-243.ap-northeast-1.compute.internal>
2022-12-24 08:26:40 +0000

    [fix]画像表示不具合修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,15 +4,6 @@ class Book < ApplicationRecord
   validates :body, presence: true
   validates :body, length: { maximum: 200 }
   
-  has_one_attached :profile_image
   book = belongs_to :user
-
-  def get_profile_image
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    profile_image.variant(resize_to_limit: [width, height]).processed
-  end
   
 end
